d6b21e3cb837ada30e5059cd74b6e5e474fd4851
Shubham <salmonhumorous@gmail.com>
2023-06-28 15:16:29 +0530

    add tesseract path in app.py and remove code to save file upload

--- app.py
+++ app.py
@@ -35,6 +35,9 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
+#tesseract path on the machine (imp)
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+
 # Path of working folder on Disk
 
 def get_string(img_path):
@@ -50,13 +53,13 @@ def get_string(img_path):
     img = cv2.erode(img, kernel, iterations=1)
 
     # Write image after removed noise
-    cv2.imwrite("removed_noise.png", img)
+    # cv2.imwrite("removed_noise.png", img)
 
     #  Apply threshold to get image with only black and white
     #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
     # Write the image after apply opencv to do some ...
-    cv2.imwrite(img_path, img)
+    # cv2.imwrite(img_path, img)
 
     # Recognize text with tesseract for python
     result = pytesseract.image_to_string(Image.open(img_path))
