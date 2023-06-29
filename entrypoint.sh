dd8eb78193c38311d23e329f246df6cbac6f97f4
Shubham <salmonhumorous@gmail.com>
2023-06-29 18:42:59 +0530

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+export XDG_RUNTIME_DIR=/run/user/1001
 cd /home/shubham/image-to-text
 echo $USER >> textfile.txt
 systemctl --user restart app
