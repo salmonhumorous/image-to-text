98eb41e1f861c22623220ec8fc8f219ef4e4ac53
Shubham <salmonhumorous@gmail.com>
2023-06-30 13:36:48 +0530

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
-export XDG_RUNTIME_DIR=/run/user/1001
 cd /home/shubham/image-to-text
 echo $USER >> textfile.txt
+export XDG_RUNTIME_DIR=/run/user/1001
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+
+
 systemctl --user restart app
 
